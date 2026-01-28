c6af4ee737d745de40d447821f99e4f0aed2692e
Vedprakash Umare <madhurumare2003@gmail.com>
2026-01-28 13:54:40 +0530

    Repository AuditLogRepo

---
+++ etms-backend/src/main/java/com/etms/repository/AuditLogRepository.java
@@ -0,0 +1,11 @@
+package com.etms.repository;
+
+import com.etms.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+
+    List<AuditLog> findByUserEmail(String userEmail);
+}
